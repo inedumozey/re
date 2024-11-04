e0b0dc241d9798c082e529f8fd56bdc7c7279cba
inedumozey <inedumozey@gmail.com>
2024-11-04 03:14:32 +0100

    updated

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -82,7 +82,7 @@ export default function Modal(
                                     return '100%'
                                 }
                                 else {
-                                    return '-150%'
+                                    return '-100%'
                                 }
                             }
                             else {
